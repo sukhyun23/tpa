aef077f69f8ed36de1231320325bc45f675137fb
sukhyun23 <shkwon23@gmail.com>
2018-08-18 21:40:35 +0900

    function to detect misspelled error

--- R/FindSimilarString.R
+++ R/FindSimilarString.R
@@ -14,20 +14,21 @@ FindSimilarString <- function(x, FR = 0.2, ignore.case = T){
   acc_dat <- as.data.frame(acc_matrix)
   acc_dat$nchar <- nchar(rownames(acc_dat))
   result <- list()
-  for (i in 1:nrow(acc_dat)) {
+  for(i in 1:nrow(acc_dat)){
     result[[i]] <- rownames(acc_dat[acc_dat[[i]] <= round(acc_dat$nchar *
                                                             FR), ])
   }
   result <- unique(result[unlist(lapply(result, function(x) ifelse(length(x) > 1, T, F)))])
-  if (length(result) == 0)
+  if(length(result) == 0)
     return(message("No problems found on this variable."))
   plotdat <- data.frame(table(x[x %in% unlist(result)]), stringsAsFactors = F)
   plotdat$fill <- NA
-  for (i in 1:length(result)) {
+  for(i in 1:length(result)){
     plotdat$fill[plotdat$Var1 %in% result[[i]]] <- i
   }
-  g <- ggplot2::ggplot(plotdat) + ggplot2::geom_bar(ggplot2::aes(x = reorder(Var1, plotdat$fill),
-                                      fill = factor(fill), y = Freq), stat = "identity") +
+  g <- ggplot2::ggplot(plotdat) +
+    ggplot2::geom_bar(ggplot2::aes(x = reorder(Var1, plotdat$fill),
+                                   fill = factor(fill), y = Freq), stat = "identity") +
     ggplot2::labs(x = "Values", y = "Freq") + ggplot2::guides(fill = F) + ggplot2::coord_flip()
   cat("Variable Summary")
   cat("\nCardinality :", length(na.omit(unique(x))))
