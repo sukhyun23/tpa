319e03736ef18c753406982de18e94618946a31e
sukhyun23 <shkwon23@gmail.com>
2018-12-17 20:19:03 +0900

    remove NA function

---
+++ R/RemoveNA.R
@@ -0,0 +1,52 @@
+# create s3 class
+RemoveNA <- function(x) UseMethod('RemoveNA')
+
+# remove column of row with missing values more than the ratio
+RemoveNA.default <- function(data, axis = 1, ratio = 0.5) {
+  n <- nrow(data)
+  p <- ncol(data)
+
+  if(axis == 1) { # row
+    NA_ratio <- apply(data, 1, function(x) sum(is.na(x))/p)
+    NA_idx <- which(NA_ratio >= ratio)
+    if(length(NA_idx) == 0) {
+      NA_idx <- -(1:n)
+    }
+    data <- data[-NA_idx, ]
+  } else if(axis == 2) { # column
+    NA_ratio <- colSums(is.na(data))/n
+    NA_idx <- which(NA_ratio >= ratio)
+    if(length(NA_idx) == 0) {
+      NA_idx <- -(1:p)
+    }
+    data <- data[, -NA_idx]
+  }
+  return(data)
+}
+
+
+# data.table class
+RemoveNA.data.table <- function(data, axis = 1, ratio = 0.5) {
+  n <- nrow(data)
+  p <- ncol(data)
+
+  if(axis == 1) { # row
+    NA_ratio <- apply(data, 1, function(x) sum(is.na(x))/p)
+    NA_idx <- which(NA_ratio >= ratio)
+    if(length(NA_idx) == 0) {
+      NA_idx <- -(1:n)
+    }
+    data <- data[-NA_idx, ]
+  } else if(axis == 2) { # column
+    NA_ratio <- colSums(is.na(data))/n
+    NA_idx <- which(NA_ratio >= ratio)
+    if(length(NA_idx) == 0) {
+      NA_idx <- -(1:p)
+    }
+    data <- data[, -NA_idx, with = F]
+  }
+  return(data)
+}
+data <- data.table(airquality)
+
+
--- R/handlingdf.R
+++ R/handlingdf.R
@@ -3,26 +3,3 @@ NAratio <- function(data) {
   ndata <- nrow(data)
   sapply(data, function(column) sum(is.na(column))/ndata)
 }
-
-# remove column of row with missing values more than the ratio
-RemoveNA <- function(data, axis = 1, ratio = 0.5) {
-  n <- nrow(data)
-  p <- ncol(data)
-
-  if(axis == 1) { # row
-    NA_ratio <- apply(data, 1, function(x) sum(is.na(x))/n)
-    NA_idx <- which(NA_ratio >= ratio)
-    if(length(NA_idx) == 0) {
-      NA_idx <- -(1:n)
-    }
-    data <- data[-NA_idx, ]
-  } else if(axis == 2) {
-    NA_ratio <- colSums(is.na(data))/n
-    NA_idx <- which(NA_ratio >= ratio)
-    if(length(NA_idx) == 0) {
-      NA_idx <- -(1:p)
-    }
-    data <- data[, -NA_idx]
-  }
-  return(data)
-}
