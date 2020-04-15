14c2ba9fb4b6faf033fb2a26d2d6aae62c907975
sukhyun23 <shkwon23@gmail.com>
2020-04-15 00:04:42 +0000

    test commit

--- R/class2.R
+++ R/class2.R
@@ -2,14 +2,17 @@ is.quan <- function(x) {
   class_x <- class(x)
   result <- any(class_x %in% c('numeric', 'integer'))
 }
+
 is.qual <- function(x) {
   class_x <- class(x)
   result <- any(class_x %in% c('character', 'factor', 'ordered'))
 }
+
 is.date <- function(x) {
   class_x <- class(x)
   result <- any(class_x %in% c('Date', 'POSIXct', 'POSIXlt', 'POSIXt'))
 }
+
 class2 <- function(x) {
   if (is.quan(x)) return('quantitative')
   if (is.qual(x)) return('qualitative')
